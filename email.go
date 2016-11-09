bbd302bbde4b67c7c5932006fcdd9681c9aa9716
Steve Manuel <nilslice@gmail.com>
2016-11-09 11:34:57 -0800

    refactoring to remove goroutine and call in sequential style - TODO add context to return from Send if send hangs, or from newClient in case connection attempts hang

--- email.go
+++ email.go
@@ -21,92 +21,92 @@ var (
 
 // Send sends a message to recipient(s) listed in the 'To' field of a Message
 func (m Message) Send() error {
-	var addrs []*net.MX
-	var err error
-
-	if strings.Contains(m.To, "@") {
-		host := strings.Split(m.To, "@")[1]
-		addrs, err = net.LookupMX(host)
-		if err != nil {
-			return err
-		}
-
-	} else {
+	if !strings.Contains(m.To, "@") {
 		return fmt.Errorf("Invalid recipient address: <%s>", m.To)
 	}
 
-	clientChan := make(chan *smtp.Client)
-	errChan := make(chan error)
+	host := strings.Split(m.To, "@")[1]
+	addrs, err := net.LookupMX(host)
+	if err != nil {
+		return err
+	}
 
-	go func(mx []*net.MX) {
-	MX:
-		for i := range mx {
-			for j := range ports {
-				server := strings.TrimSuffix(mx[i].Host, ".")
-				hostPort := fmt.Sprintf("%s:%d", server, ports[j])
-				client, err := smtp.Dial(hostPort)
-				if err != nil {
-					if j == len(ports)-1 {
-						errChan <- err
-						return
-					}
+	c, err := newClient(addrs, ports)
+	if err != nil {
+		return err
+	}
 
-					continue
+	err = send(m, c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func newClient(mx []*net.MX, ports []int) (*smtp.Client, error) {
+	for i := range mx {
+		for j := range ports {
+			server := strings.TrimSuffix(mx[i].Host, ".")
+			hostPort := fmt.Sprintf("%s:%d", server, ports[j])
+			client, err := smtp.Dial(hostPort)
+			if err != nil {
+				if j == len(ports)-1 {
+					return nil, err
 				}
 
-				clientChan <- client
-				break MX
+				continue
 			}
-		}
-	}(addrs)
 
-	select {
-	case c := <-clientChan:
-		if err := c.Mail(m.From); err != nil {
-			return err
+			return client, nil
 		}
+	}
 
-		if err := c.Rcpt(m.To); err != nil {
-			return err
-		}
+	return nil, fmt.Errorf("Coudln't connect to servers %v on any common port.", mx)
+}
 
-		msg, err := c.Data()
+func send(m Message, c *smtp.Client) error {
+	if err := c.Mail(m.From); err != nil {
+		return err
+	}
+
+	if err := c.Rcpt(m.To); err != nil {
+		return err
+	}
+
+	msg, err := c.Data()
+	if err != nil {
+		return err
+	}
+
+	if m.Subject != "" {
+		_, err = msg.Write([]byte("Subject: " + m.Subject + "\r\n"))
 		if err != nil {
 			return err
 		}
+	}
 
-		if m.Subject != "" {
-			_, err = msg.Write([]byte("Subject: " + m.Subject + "\r\n"))
-			if err != nil {
-				return err
-			}
-		}
-
-		if m.From != "" {
-			_, err = msg.Write([]byte("From: " + m.From + "\r\n"))
-			if err != nil {
-				return err
-			}
-		}
-
-		if m.To != "" {
-			_, err = msg.Write([]byte("To: " + m.To + "\r\n"))
-			if err != nil {
-				return err
-			}
-		}
-
-		_, err = fmt.Fprint(msg, m.Body)
+	if m.From != "" {
+		_, err = msg.Write([]byte("From: " + m.From + "\r\n"))
 		if err != nil {
 			return err
 		}
+	}
 
-		err = msg.Close()
+	if m.To != "" {
+		_, err = msg.Write([]byte("To: " + m.To + "\r\n"))
 		if err != nil {
 			return err
 		}
+	}
 
-	case err := <-errChan:
+	_, err = fmt.Fprint(msg, m.Body)
+	if err != nil {
+		return err
+	}
+
+	err = msg.Close()
+	if err != nil {
 		return err
 	}
 
